fccdb2b72fdfdd940416be1ac01cf200fa47ba27
Dipuo Mahlake <dipuomahlake060@gmail.com>
2021-07-23 16:14:15 +0200

    initial commit

---
+++ Library/Main.java
@@ -0,0 +1,10 @@
+package Library;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        User customer = new User();
+        customer.display();
+    }
+}
---
+++ Library/book.java
@@ -0,0 +1,15 @@
+package Library;
+
+public class book {
+    public int bookId;
+    private String bookName;
+    private double bookPrice;
+    private String author;
+    private String status;
+
+    public void displayBookDetails()
+    {
+
+    }
+
+}
